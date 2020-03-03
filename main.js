2fbbbe93e96d0f04307c835d036118ba82009f65
Pawel Nowak <pawel.nowak.90@gmail.com>
2020-03-03 10:46:36 +0100

    My-first-commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const btnScrollUp = document.querySelector('div.button-scroll');
+btnScrollUp.addEventListener('click', scrollToUp);
+
+
+function scrollToUp(e) {
+    e.preventDefault();
+    // setTimeout(() => {
+    //         console.log(document.scrollTop(), 1000);
+    //     }
+}
+
+const div = document.querySelector('div.container');
+console.log(+);
