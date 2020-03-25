ce9eeb7e745e86508a696c5272b9d6f28b3b8d90
Pawel Nowak <pawel.nowak.90@gmail.com>
2020-03-25 17:17:22 +0100

    Sekcja o-mnie połączona z nawigacja

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btnScrollUp = document.querySelector('div.button-scroll');
-
-
+const nav = document.getElementById('menu');
+// const sectionAboutMe = document.getElementsByClassName('o-mnie'); - pobieram w ten sposób HTML collections i .offsetHeight zwraca undefined !
+const sectionAboutMe = document.querySelector('section.o-mnie');
 
 // function scrollToUp(e) {
 //     e.preventDefault();
@@ -13,35 +14,39 @@ const scrollTopBar = document.querySelector('div.loading-bar');
 scrollTopBar.style.width = 0 + "px";
 let scrollValue = 0;
 
+
+
 //okreslenie przewiniecia strony w procentach
-// var sbHeight = window.innerHeight * (window.innerHeight / document.body.offsetHeight);
 var totalHeight = document.body.offsetHeight;
 var actualScroll = window.scrollY;
-var maxiScrollY = (document.body.offsetHeight - window.innerHeight);
-var precentageHeight;
-
+var maxiScrollY = (document.body.offsetHeight - window.innerHeight); //tu są jakieś błędy rzędu 20-30px ? może window inner height nie uwzględnia marginesów ? albo nie uwzględniam marigunesu który potem dodaję ??
 window.addEventListener('scroll', (e) => {
     precentageHeight = window.scrollY / maxiScrollY;
     scrollTopBar.style.width = precentageHeight * 100 + "%";
+    // console.log(Math.floor(precentageHeight * 100) + "%");
 });
 
-
-
+// MENU dodanie .active po przewinięciu 100vh
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= sectionAboutMe.offsetHeight) {
+        nav.classList.add("active");
+        console.log(window.scrollY);
+        console.log('Teraz dodaję klasę .active do nav-MENU');
+    } else if (window.scrollY <= sectionAboutMe.offsetHeight) {
+        nav.classList.remove("active");
+    }
+});
 
 //sprawdzenie wysokości nawigacji NAV i ustawienie właściwego poddingu na element następny
 function checkNavHeight() {
     if (window.innerWidth <= 2600) {
-        const navHeight = document.getElementById('nav').offsetHeight + "px";
+        const navHeight = document.getElementById('menu').offsetHeight + "px";
         // document.getElementById('main').style.padding = `${navHeight} 0 0 0`;
-        console.log('nastapil event resize, wartość padding = ' + navHeight);
+        // console.log('nastapil event resize, wartość padding = ' + navHeight);
     }
 }
 window.addEventListener('load', checkNavHeight);
 window.addEventListener('resize', checkNavHeight);
-
-
-
-
 
 
 // efekt fade in/schowania przycisku button przenoszącego na górę strony
@@ -50,16 +55,16 @@ let flag = 0;
 function fadeInButton() {
     if ((window.scrollY >= 80) && (!flag)) {
         btnScrollUp.classList.add('active');
-        console.log('dla button scrollUp dodano klase active');
+        // console.log('dla button scrollUp dodano klase active');
         flag = !flag;
-        console.log('wartosc flag :' + flag);
-        console.log(window.scrollY);
+        // console.log('wartosc flag :' + flag);
+        // console.log(window.scrollY);
     } else if ((window.scrollY < 300) && (flag)) {
         btnScrollUp.classList.remove('active');
-        console.log('dla button scrollUp zabrano klase active');
+        // console.log('dla button scrollUp zabrano klase active');
         flag = !flag;
-        console.log('wartosc flag :' + flag);
-        console.log(window.scrollY);
+        // console.log('wartosc flag :' + flag);
+        // console.log(window.scrollY);
     }
 }
 
@@ -73,7 +78,5 @@ function fadeInButton() {
 //     }
 // }
 
-
 // window.addEventListener('scroll', fadeInButton);
-
 $(window).on('scroll', fadeInButton);
