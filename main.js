381bb88b7180173c06c6e3e2dd95839d697255bf
Pawel Nowak <pawel.nowak.90@gmail.com>
2020-04-16 18:35:36 +0200

    Niepełna-ale-dzialajaca-refaktoryzacja

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btnScrollUp = document.querySelector('div.button-scroll');
+const btnToTop = document.querySelector('a.to-top');
 const nav = document.getElementById('navigation');
 // const sectionAboutMe = document.getElementsByClassName('o-mnie'); - pobieram w ten sposób HTML collections i .offsetHeight zwraca undefined !
 const sectionAboutMe = document.querySelector('section.o-mnie');
@@ -6,7 +6,6 @@ const sectionTechnologies = document.querySelector('section.technologie');
 const educationRight = document.getElementById('education-POLLUB');
 const educationLeft = document.getElementById('education-AGH');
 const techCircSkills = [...document.querySelectorAll('div.one-skill')];
-
 
 //Pasek postepu przewijania
 // const div = document.querySelector('div.container');
@@ -16,11 +15,13 @@ scrollTopBar.style.width = 0 + "px";
 var totalHeight = document.body.offsetHeight;
 var actualScroll = window.scrollY;
 
-
-window.addEventListener('scroll', (e) => {
-    precentageHeight = (window.scrollY) / document.body.offsetHeight;
+function progressBar(e) {
+    precentageHeight = (window.scrollY) / (document.body.offsetHeight - window.innerHeight);
     scrollTopBar.style.width = precentageHeight * 100 + "%";
-});
+}
+
+window.addEventListener('scroll', progressBar);
+window.onload = progressBar;
 
 // NAV fixed - add OR remove .active po przewinięciu 100vh
 //ta wartosc powinna byc zależna od aktualnej wysokosci przegladarki ?!
@@ -32,31 +33,38 @@ window.addEventListener('scroll', () => {
     }
 });
 
-//sekcja wyksztalcenie, - add .active - zalaczenia animacji wjazdu z boku
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= (sec_0 + sectionTechnologies.offsetHeight)) {
-        educationRight.classList.add("active");
-        educationLeft.classList.add("active");
-    }
-});
-
 //DODAĆ FLAGĘ ŻEBY TO SIĘ W KÓŁKO NIE WŁĄCZAŁO ...
 
 // efekt fade in/schowania przycisku button przenoszącego na górę strony
 let flag = 0;
 
 function fadeInButton() {
-    if ((window.scrollY >= 80) && (!flag)) {
-        btnScrollUp.classList.add('active');
+    // if ((window.pageYOffset >= 120) && (!flag)) {
+    if ((window.scrollY >= 120) && (!flag)) {
+        btnToTop.classList.add('active');
         flag = !flag;
-    } else if ((window.scrollY < 300) && (flag)) {
-        btnScrollUp.classList.remove('active');
+    } else if ((window.scrollY < 120) && (flag)) {
+        btnToTop.classList.remove('active');
         flag = !flag;
     }
 }
 
 // window.addEventListener('scroll', fadeInButton);
 $(window).on('scroll', fadeInButton);
+
+// $(document).ready(function () {
+//     var offset = 250;
+//     var duration = 500;
+
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > offset) {
+//             $('.to-top').fadeIn(duration);
+//         } else {
+//             $('.to-top').fadeOut(duration);
+//         }
+//     })
+
+// });
 
 //pojawianie się nagłówków h1 na scrollY - rozwiazanie trzeba zoptymalizować
 const h1_1OMnie = document.querySelector('section#o-mnie h1');
@@ -66,17 +74,6 @@ const h1_4DosZawodowe = document.querySelector('section#doswiadczenie-zawodowe h
 const h1_5InneUm = document.querySelector('section#inne-umiejetnosci h1');
 const h1_6MojeZainteresowania = document.querySelector('section#moje-zainteresowania h1');
 const h1_7Kontakt = document.querySelector('section#kontakt h1');
-
-
-
-const sec_0 = document.querySelector('section#my-baner').offsetHeight;
-const sec_1 = document.querySelector('section#o-mnie').offsetHeight;
-const sec_2 = document.querySelector('section#technologie').offsetHeight;
-const sec_3 = document.querySelector('section#inne-umiejetnosci').offsetHeight;
-const sec_4 = document.querySelector('section#wyksztalcenie').offsetHeight;
-const sec_5 = document.querySelector('section#doswiadczenie-zawodowe').offsetHeight;
-const sec_6 = document.querySelector('section#moje-zainteresowania').offsetHeight;
-const sec_7 = document.querySelector('section#kontakt').offsetHeight;
 
 const kontaktLeftAnimation = document.querySelector('div.kontakt-left');
 const kontaktRightAnimation = document.querySelector('div.kontakt-right');
@@ -88,72 +85,107 @@ const secOMnieP = [...document.querySelectorAll('section#o-mnie p')];
 
 //zdarzenia na scroll oraz onload - naglowki i animacje
 
+//PAWEL PAWEL jak to zaimplementować na scroll ?
+//pomysl drugi dodawać w pętli
+// $(document).scroll(function (event) {
+//     var offset = $(this).offset().top;
+//     event.stopPropagation();
+//     console.log(offset);
+//     console.log('dziala scoolrlrlr');
+
+// });
+
+const sec_0 = document.querySelector('section#my-baner').offsetHeight;
+const sec_1 = document.querySelector('section#o-mnie').offsetHeight;
+const sec_2 = document.querySelector('section#technologie').offsetHeight;
+const sec_3 = document.querySelector('section#inne-umiejetnosci').offsetHeight;
+const sec_4 = document.querySelector('section#wyksztalcenie').offsetHeight;
+const sec_5 = document.querySelector('section#doswiadczenie-zawodowe').offsetHeight;
+const sec_6 = document.querySelector('section#moje-zainteresowania').offsetHeight;
+const sec_7 = document.querySelector('section#kontakt').offsetHeight;
+
 function scrollAndOnloadEvent() {
-    if (window.scrollY >= sec_1 * 0.5) {
+    if (window.scrollY >= document.querySelector('section#o-mnie').offsetTop - 2 * navHeight) {
         h1_1OMnie.classList.add('active');
         secOMnieP.forEach(p => p.classList.add('active'));
     }
-    if (window.scrollY >= sec_0 + sec_1 * 0.5) {
+    if (window.scrollY >= document.querySelector('section#technologie').offsetTop - 2 * navHeight) {
         h1_2Technologie.classList.add('active');
         techCircSkills.forEach(techCircSkill => techCircSkill.classList.add("active"));
     }
 
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 * 0.5) h1_3Wykszt.classList.add('active');
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 * 0.5) {
-        h1_4DosZawodowe.classList.add('active');
-        secDoswiadczenieZawodoweP.forEach(p => p.classList.add('active'));
-
-    }
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 * 0.5 /*+ sec_3 + sec_4 * 0.5*/ ) {
+    if (window.scrollY >= document.querySelector('section#inne-umiejetnosci').offsetTop - 2 * navHeight) {
         h1_5InneUm.classList.add('active');
         secInneUmiejetnosciP.forEach(p => p.classList.add('active'));
     }
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 * 0.2) h1_6MojeZainteresowania.classList.add('active');
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 * 0.2) h1_7Kontakt.classList.add('active');
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 * 0.2) kontaktLeftAnimation.classList.add('active');
-    if (window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 * 0.2) kontaktRightAnimation.classList.add('active');
+    if (window.scrollY >= document.querySelector('section#wyksztalcenie').offsetTop - 2 * navHeight) {
+        h1_3Wykszt.classList.add('active');
+        educationRight.classList.add("active");
+        educationLeft.classList.add("active");
+
+    }
+    if (window.scrollY >= document.querySelector('section#doswiadczenie-zawodowe').offsetTop - 2 * navHeight) {
+        h1_4DosZawodowe.classList.add('active');
+        secDoswiadczenieZawodoweP.forEach(p => p.classList.add('active'));
+    }
+    if (window.scrollY >= document.querySelector('section#moje-zainteresowania').offsetTop - 2 * navHeight) h1_6MojeZainteresowania.classList.add('active');
+    // .. to jest ostatnia sekcja więc warunek jest ulożony inaczej
+    if (window.scrollY >= document.querySelector('section#kontakt').offsetTop - 7 * navHeight) h1_7Kontakt.classList.add('active');
+    if (window.scrollY >= document.querySelector('section#kontakt').offsetTop - 7 * navHeight) kontaktLeftAnimation.classList.add('active');
+    if (window.scrollY >= document.querySelector('section#kontakt').offsetTop - 7 * navHeight) kontaktRightAnimation.classList.add('active');
 }
 
 
-function navNowOnScreen() {
-    console.log(window.scrollY);
+// document.querySelector('section#my-baner').offsetTop - navHeight
+// document.querySelector('section#o-mnie').offsetTop - navHeight
+// document.querySelector('section#technologie').offsetTop - navHeight
+// document.querySelector('section#inne-umiejetnosci').offsetTop - navHeight
+// document.querySelector('section#wyksztalcenie').offsetTop - navHeight
+// document.querySelector('section#doswiadczenie-zawodowe').offsetTop - navHeight
+// document.querySelector('section#moje-zainteresowania').offsetTop - navHeight
+// document.querySelector('section#kontakt').offsetTop - navHeight
+const navHeight = document.getElementById('navigation').offsetHeight;
 
-    if ((window.scrollY >= sec_0 * 0.8) && (window.scrollY <= sec_0 + sec_1 * 0.8)) {
+function navNowOnScreen() {
+    // console.log(window.scrollY);
+
+    if ((window.scrollY >= document.querySelector('section#o-mnie').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#technologie').offsetTop - navHeight)) {
         document.querySelector('nav li:nth-of-type(1) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(1) a.nav-link').classList.remove('nowOnScreen');
     }
 
-    if ((window.scrollY >= sec_0 + sec_1 * 0.8) && (window.scrollY <= sec_0 + sec_1 + sec_2 * 0.8)) {
+    if ((window.scrollY >= document.querySelector('section#technologie').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#inne-umiejetnosci').offsetTop - navHeight)) {
         document.querySelector('nav li:nth-of-type(2) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(2) a.nav-link').classList.remove('nowOnScreen');
     }
 
-    if ((window.scrollY >= sec_0 + sec_1 + sec_2 * 0.8) && (window.scrollY <= sec_0 + sec_1 + sec_2 + sec_3 * 0.8)) {
+    if ((window.scrollY >= document.querySelector('section#inne-umiejetnosci').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#wyksztalcenie').offsetTop - navHeight)) {
         document.querySelector('nav li:nth-of-type(3) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(3) a.nav-link').classList.remove('nowOnScreen');
     }
 
-    if ((window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 * 0.8) && (window.scrollY <= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 * 0.8)) {
+    if ((window.scrollY >= document.querySelector('section#wyksztalcenie').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#doswiadczenie-zawodowe').offsetTop - navHeight)) {
         document.querySelector('nav li:nth-of-type(4) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(4) a.nav-link').classList.remove('nowOnScreen');
     }
 
-    if ((window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 * 0.8) && (window.scrollY <= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 * 0.8)) {
+    if ((window.scrollY >= document.querySelector('section#doswiadczenie-zawodowe').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#moje-zainteresowania').offsetTop - navHeight)) {
         document.querySelector('nav li:nth-of-type(5) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(5) a.nav-link').classList.remove('nowOnScreen');
     }
 
-    if ((window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 * 0.8) && (window.scrollY <= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 * 0.25)) {
+    if ((window.scrollY >= document.querySelector('section#moje-zainteresowania').offsetTop - navHeight) && (window.scrollY < document.querySelector('section#kontakt').offsetTop - 7 * navHeight)) {
         document.querySelector('nav li:nth-of-type(6) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(6) a.nav-link').classList.remove('nowOnScreen');
     }
-    if ((window.scrollY >= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 * 0.25) && (window.scrollY <= sec_0 + sec_1 + sec_2 + sec_3 + sec_4 + sec_5 + sec_6 + sec_7)) {
+    // .. to jest ostatnia sekcja więc warunek jest ulożony inaczej
+    if ((window.scrollY >= document.querySelector('section#kontakt').offsetTop - 7 * navHeight) && (window.scrollY < document.querySelector('section#kontakt').offsetTop - navHeight + document.querySelector('section#kontakt').offsetHeight)) {
         document.querySelector('nav li:nth-of-type(7) a.nav-link').classList.add('nowOnScreen');
     } else {
         document.querySelector('nav li:nth-of-type(7) a.nav-link').classList.remove('nowOnScreen');
@@ -167,3 +199,15 @@ $(window).on('scroll', navNowOnScreen);
 $(window).on('scroll', scrollAndOnloadEvent);
 window.onload = scrollAndOnloadEvent;
 window.onload = scrollAndOnloadEvent;
+
+
+//-----------------------------------------------------------------------
+const navA = [...document.querySelectorAll('nav a.nav-link')];
+
+const body = document.documentElement;
+
+navA.forEach((item) => item.addEventListener('click', function (event) {
+    event.preventDefault();
+    let sectionId = this.hash.slice(1);
+    body.scrollTop = document.getElementById(sectionId).offsetTop - navHeight;
+}));
